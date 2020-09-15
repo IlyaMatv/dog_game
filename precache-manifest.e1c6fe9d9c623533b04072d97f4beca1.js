f6a7b9c0a074f8a4392941670401eff8ec8299c0
Ilya Matv <arkashabi4@gmail.com>
2020-09-15 19:07:27 +0300

    Updates

---
+++ precache-manifest.e1c6fe9d9c623533b04072d97f4beca1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "862e90e7e5b76e48e46cfc15455d907e",
+    "url": "/dog_game/index.html"
+  },
+  {
+    "revision": "910719757a6eefa2b399",
+    "url": "/dog_game/static/css/main.92bb7053.chunk.css"
+  },
+  {
+    "revision": "6d82510cde9a790d1087",
+    "url": "/dog_game/static/js/2.1240be0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dog_game/static/js/2.1240be0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "910719757a6eefa2b399",
+    "url": "/dog_game/static/js/main.67d13f5c.chunk.js"
+  },
+  {
+    "revision": "f227734bbcff0d04791b",
+    "url": "/dog_game/static/js/runtime-main.c4b09433.js"
+  },
+  {
+    "revision": "75fa68d4d95a95385dabee5a76f50797",
+    "url": "/dog_game/static/media/dog.75fa68d4.mp3"
+  }
+]);
